c4eb32a447dc2035c06709b9c1ed778926737125
daengdaengLee <gunho1020@gmail.com>
2023-09-01 00:51:55 +0900

    feat: 문자열을 해싱하는 HashService 개발
    - MD5 알고리즘 선택
    - 입력 문자열만 받거나, salt 값을 추가로 받을 수 있도록 2가지 메소드 제공

---
+++ src/main/java/io/github/daengdaenglee/mangurl/application/mangle/service/HashService.java
@@ -0,0 +1,31 @@
+package io.github.daengdaenglee.mangurl.application.mangle.service;
+
+import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+@Service
+public class HashService {
+    private final MessageDigest md;
+
+    HashService() {
+        try {
+            this.md = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    byte[] hash(String message) {
+        this.md.update(message.getBytes(StandardCharsets.UTF_8));
+        return md.digest();
+    }
+
+    byte[] hash(String message, String salt) {
+        var saltedMessage = message + salt;
+        this.md.update(saltedMessage.getBytes(StandardCharsets.UTF_8));
+        return md.digest();
+    }
+}
