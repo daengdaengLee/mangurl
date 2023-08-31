2c7be3664659caa454e16d3c96aeeca2de6f79f7
daengdaengLee <gunho1020@gmail.com>
2023-09-01 00:53:33 +0900

    test: HashService 테스트 작성
    - MD5 알고리즘으로 해싱한 결과가 같은지 검사
    - 해싱할 메시지만 입력할 때, salt 값을 같이 입력할 때 모두 테스트

---
+++ src/test/java/io/github/daengdaenglee/mangurl/application/mangle/service/HashServiceTest.java
@@ -0,0 +1,53 @@
+package io.github.daengdaenglee.mangurl.application.mangle.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class HashServiceTest {
+    private MessageDigest md;
+
+    @BeforeEach
+    void beforeEach() throws NoSuchAlgorithmException {
+        this.md = MessageDigest.getInstance("MD5");
+    }
+
+
+    @Test
+    @DisplayName("입력한 메시지를 MD5 알고리즘으로 해싱한 결과를 바이트 배열로 반환한다.")
+    void hash() {
+        // given
+        var message = "https://google.com";
+        var hashService = new HashService();
+
+        // when
+        var hashed = hashService.hash(message);
+
+        // then
+        this.md.update(message.getBytes());
+        var expected = this.md.digest();
+        assertThat(hashed).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("입력한 메시지에 소금을 쳐서 MD5 알고리즘으로 해싱한 결과를 바이트 배열로 반환한다.")
+    void hashWithSalt() {
+        // given
+        var salt = "THIS_IS_SALT";
+        var message = "https://google.com";
+        var hashService = new HashService();
+
+        // when
+        var hashed = hashService.hash(message, salt);
+
+        // then
+        this.md.update((message + salt).getBytes());
+        var expected = this.md.digest();
+        assertThat(hashed).isEqualTo(expected);
+    }
+}
