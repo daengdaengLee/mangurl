920abe645aa9f7d730b2e60f0e6a54f324eceb5c
daengdaengLee <gunho1020@gmail.com>
2023-09-01 17:31:38 +0900

    refactor: BaseXEncodeService 를 62진법에 특화된 형태로 수정
    - 서비스에서 62진법 변환 이외의 경우가 없을 것으로 예상

--- src/test/java/io/github/daengdaenglee/mangurl/application/mangle/service/Base62EncodeServiceTest.java
+++ src/test/java/io/github/daengdaenglee/mangurl/application/mangle/service/Base62EncodeServiceTest.java
@@ -8,13 +8,13 @@ import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class BaseXEncodeServiceTest {
+class Base62EncodeServiceTest {
     @Test
     @DisplayName("빈 byte 배열을 입력하면 빈 문자열을 반환한다.")
     void encodeBase62Empty() {
         // given
         var bytes = new byte[]{};
-        var baseXEncodeService = new BaseXEncodeService();
+        var baseXEncodeService = new Base62EncodeService();
 
         // when
         var encoded = baseXEncodeService.encode(bytes);
@@ -27,10 +27,9 @@ class BaseXEncodeServiceTest {
     @DisplayName("입력한 byte 배열을 62진법으로 변환한다.")
     void encodeBase62() {
         // given
-        var codec = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
         var expected = "aB3";
-        var bytes = this.toByteArray(codec, expected);
-        var baseXEncodeService = new BaseXEncodeService();
+        var bytes = this.toByteArray(expected);
+        var baseXEncodeService = new Base62EncodeService();
 
         // when
         var encoded = baseXEncodeService.encode(bytes);
@@ -39,28 +38,12 @@ class BaseXEncodeServiceTest {
         assertThat(encoded).isEqualTo(expected);
     }
 
-    @Test
-    @DisplayName("입력한 byte 배열을 16진법으로 변환한다.")
-    void encodeBase16() {
-        // given
-        var codec = "0123456789abcdef";
-        var expected = "ab3";
-        var bytes = this.toByteArray(codec, expected);
-        var baseXEncodeService = new BaseXEncodeService(codec);
-
-        // when
-        var encoded = baseXEncodeService.encode(bytes);
-
-        // then
-        assertThat(encoded).isEqualTo(expected);
-    }
-
-    private byte[] toByteArray(String codec, String value) {
-        var codecList = codec.chars()
+    private byte[] toByteArray(String expected) {
+        var codecList = Base62EncodeService.CODEC.chars()
                 .mapToObj(c -> (char) c)
                 .toList();
         var base = codecList.size();
-        var charArray = value.toCharArray();
+        var charArray = expected.toCharArray();
         return IntStream.range(0, charArray.length)
                 .mapToObj(i -> {
                     var c = charArray[i];
