56f79a93b0f42cb8cbe3a5d99986163c2e41ad5c
daengdaengLee <gunho1020@gmail.com>
2023-09-05 16:12:00 +0900

    docs: ShortenUrlService#shortenUrl 에서 발생할 수 있는 예외 케이스 작성

--- src/main/java/io/github/daengdaenglee/mangurl/application/url/inboundport/ShortenUrlService.java
+++ src/main/java/io/github/daengdaenglee/mangurl/application/url/inboundport/ShortenUrlService.java
@@ -1,5 +1,8 @@
 package io.github.daengdaenglee.mangurl.application.url.inboundport;
 
 public interface ShortenUrlService {
+    /**
+     * @throws RuntimeException originalUrl 에 대한 shortUrlCode 를 해시 충돌 때문에 저장할 수 없는 경우
+     */
     String shortenUrl(String originalUrl);
 }
