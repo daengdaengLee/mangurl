768650678f92f5b2004f5a164928b50fffc4768a
daengdaengLee <gunho1020@gmail.com>
2023-09-06 14:56:15 +0900

    feat: app 화면 초기화

--- src/main/java/io/github/daengdaenglee/mangurl/inboundadapter/web/WebController.java
+++ src/main/java/io/github/daengdaenglee/mangurl/inboundadapter/web/WebController.java
@@ -5,6 +5,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.server.ResponseStatusException;
@@ -20,5 +21,10 @@ public class WebController {
                 .map(response::encodeRedirectURL)
                 .map(redirectUrl -> "redirect:" + redirectUrl)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @GetMapping("/app")
+    public String app() {
+        return "app";
     }
 }
