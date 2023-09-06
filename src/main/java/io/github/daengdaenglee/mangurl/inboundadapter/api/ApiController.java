bfe68c33b8f67be90a635563cbcd1ad758c17d3d
daengdaengLee <gunho1020@gmail.com>
2023-09-06 14:25:15 +0900

    feat: URL 단축 API 엔드포인트 개발
    - origin 주소 일단 하드코딩

---
+++ src/main/java/io/github/daengdaenglee/mangurl/inboundadapter/api/ApiController.java
@@ -0,0 +1,27 @@
+package io.github.daengdaenglee.mangurl.inboundadapter.api;
+
+import io.github.daengdaenglee.mangurl.application.url.inboundport.ShortenUrlService;
+import io.github.daengdaenglee.mangurl.inboundadapter.api.request.ShortenRequest;
+import io.github.daengdaenglee.mangurl.inboundadapter.api.response.ShortenResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+    private final ShortenUrlService shortenUrlService;
+
+    @PostMapping("/shorten")
+    public ShortenResponse shorten(@RequestBody ShortenRequest shortenRequest) {
+        var shortUrl = this.shortenUrlService.shortenUrl(shortenRequest.data().url());
+        return ShortenResponse
+                .builder()
+                // @TODO origin 주소 설정으로 분리
+                .shortUrl("http://localhost:8080/" + shortUrl)
+                .build();
+    }
+}
