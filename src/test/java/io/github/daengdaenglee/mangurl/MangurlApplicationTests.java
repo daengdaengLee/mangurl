a3b56cec9fa09f89732953b94ee3adfe425412bf
daengdaengLee <gunho1020@gmail.com>
2023-09-10 21:20:34 +0900

    chore: github workflow 테스트중

---
+++ src/test/java/io/github/daengdaenglee/mangurl/MangurlApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.daengdaenglee.mangurl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangurlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
