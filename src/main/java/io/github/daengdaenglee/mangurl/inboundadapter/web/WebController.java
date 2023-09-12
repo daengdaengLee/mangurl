f386630b6e717dba423ec7ef1c53ec5de12609aa
daengdaengLee <gunho1020@gmail.com>
2023-09-12 16:44:01 +0900

    refactor: view 에서 param 직접 쓰는 곳 모두 model 사용하도록 변경

--- src/main/java/io/github/daengdaenglee/mangurl/inboundadapter/web/WebController.java
+++ src/main/java/io/github/daengdaenglee/mangurl/inboundadapter/web/WebController.java
@@ -2,19 +2,31 @@ package io.github.daengdaenglee.mangurl.inboundadapter.web;
 
 import io.github.daengdaenglee.mangurl.application.url.inboundport.RestoreUrlService;
 import io.github.daengdaenglee.mangurl.application.url.inboundport.ShortenUrlService;
+import io.github.daengdaenglee.mangurl.config.properties.MangurlProperties;
 import io.github.daengdaenglee.mangurl.inboundadapter.web.form.ShortenUrlForm;
-import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-@RequiredArgsConstructor
+@Slf4j
 @Controller
 class WebController {
+    private final String origin;
     private final ShortenUrlService shortenUrlService;
     private final RestoreUrlService restoreUrlService;
+
+    WebController(
+            MangurlProperties mangurlProperties,
+            ShortenUrlService shortenUrlService,
+            RestoreUrlService restoreUrlService) {
+        this.origin = mangurlProperties.origin();
+        this.shortenUrlService = shortenUrlService;
+        this.restoreUrlService = restoreUrlService;
+    }
 
     @RequestMapping("/{shortUrlCode}")
     String redirect(@PathVariable String shortUrlCode) {
@@ -30,11 +42,19 @@ class WebController {
     }
 
     @GetMapping("/app")
-    String app() {
+    String app(
+            @RequestParam(value = "result", required = false) String result,
+            @RequestParam(value = "resOriginalUrl", defaultValue = "") String resOriginalUrl,
+            @RequestParam(value = "resShortUrl", defaultValue = "") String resShortUrl,
+            @RequestParam(value = "prevOriginalUrl", defaultValue = "") String prevOriginalUrl,
+            Model model) {
+        model.addAttribute("isResult", result != null);
+        model.addAttribute("resOriginalUrl", resOriginalUrl);
+        model.addAttribute("resShortUrl", resShortUrl);
+        model.addAttribute("prevOriginalUrl", prevOriginalUrl);
         return "app";
     }
 
-    // @TODO 임시, 나중에 app.html 에서 JS 로 API 호출하는 형태로 변경
     @PostMapping("/app")
     String shorten(
             @ModelAttribute ShortenUrlForm shortenUrlForm,
@@ -47,7 +67,7 @@ class WebController {
             // @TODO binding result 를 이용하여 오류 메세지 보여주기
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "잘못된 URL 입니다.");
         }
-        var shortUrl = "http://localhost:8080/" + shortUrlCode;
+        var shortUrl = this.origin + shortUrlCode;
 
         redirectAttributes.addAttribute("result", "");
         redirectAttributes.addAttribute("resOriginalUrl", originalUrl);
