1abea9532cde0038b3c89c6d26701523f6150bb0
daengdaengLee <gunho1020@gmail.com>
2023-09-13 22:28:36 +0900

    fix: 설정 파일 저장할 디렉토리 초기화

--- src/main/java/io/github/daengdaenglee/mangurl/MangurlApplication.java
+++ src/main/java/io/github/daengdaenglee/mangurl/MangurlApplication.java
@@ -11,7 +11,9 @@ import software.amazon.awssdk.transfer.s3.S3TransferManager;
 import software.amazon.awssdk.transfer.s3.model.DownloadFileRequest;
 import software.amazon.awssdk.transfer.s3.progress.LoggingTransferListener;
 
+import java.io.IOException;
 import java.net.URI;
+import java.nio.file.Files;
 import java.nio.file.Paths;
 
 @SpringBootApplication
@@ -37,6 +39,14 @@ public class MangurlApplication {
         var bucket = s3Uri.bucket().orElseThrow(() -> new IllegalArgumentException("MANGURL_CONFIG_FILE_FROM URL 이 잘못되었습니다. bucket 을 알 수 없습니다."));
         var key = s3Uri.key().orElseThrow(() -> new IllegalArgumentException("MANGURL_CONFIG_FILE_FROM URL 이 잘못되었습니다. key 를 알 수 없습니다."));
 
+
+        var destinationPath = Paths.get(MANGURL_CONFIG_FILE_TO);
+        try {
+            Files.createDirectories(destinationPath.getParent());
+        } catch (IOException e) {
+            throw new RuntimeException("MANGURL_CONFIG_FILE_TO 위치에 디렉토리를 만들 수 없습니다.", e);
+        }
+
         var s3ClientBuilder = S3AsyncClient.builder();
         if (MANGURL_AWS_ACCESS_KEY_ID != null && MANGURL_AWS_SECRET_ACCESS_KEY != null) {
             var awsBasicCredentials = AwsBasicCredentials.create(MANGURL_AWS_ACCESS_KEY_ID, MANGURL_AWS_SECRET_ACCESS_KEY);
@@ -47,7 +57,7 @@ public class MangurlApplication {
              var s3TransferManager = S3TransferManager.builder().s3Client(s3Client).build()) {
             var downloadFileRequest = DownloadFileRequest.builder()
                     .getObjectRequest(req -> req.bucket(bucket).key(key))
-                    .destination(Paths.get(MANGURL_CONFIG_FILE_TO))
+                    .destination(destinationPath)
                     .addTransferListener(LoggingTransferListener.create())
                     .build();
             var download = s3TransferManager.downloadFile(downloadFileRequest);
