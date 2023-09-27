c87f0a2650ca10d126301ca39fba1f14ec50aba6
daengdaengLee <gunho1020@gmail.com>
2023-09-27 15:36:46 +0900

    refactor: S3AsyncClient 생성하는 코드 S3Config 로 분리
    - 설정 객체로 책임 분리
    - 나중에 Spring 내부에서 S3 사용할 때 꺼내 쓸 수 있음

---
+++ src/main/java/io/github/daengdaenglee/mangurl/lib/s3/S3Config.java
@@ -0,0 +1,23 @@
+package io.github.daengdaenglee.mangurl.lib.s3;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3AsyncClient;
+
+import java.util.Optional;
+
+@Configuration
+public class S3Config {
+    @Bean
+    @ConditionalOnBean(Region.class)
+    public S3AsyncClient s3AsyncClient(Region region, Optional<AwsCredentials> awsCredentials) {
+        var builder = S3AsyncClient.builder();
+        builder.region(region);
+        awsCredentials.map(StaticCredentialsProvider::create).ifPresent(builder::credentialsProvider);
+        return builder.build();
+    }
+}
